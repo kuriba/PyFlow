42e4913a594c0adc8aef678b9bb3530bfcf47225
abreha.b <abreha.b@husky.neu.edu>
2020-06-19 15:43:35 -0600

    added begin step command; comments

--- pyflow/flow/commands.py
+++ pyflow/flow/commands.py
@@ -11,11 +11,32 @@ class Commands:
         subprocess.run([])
 
     @staticmethod
-    def get_run_command(step_id: str, timelim: int):
+    def get_run_command(step_id: str, timelim: int) -> str:
+        """
+        Command used for running an array calculation for the specified step ID.
+        :param step_id:
+        :param timelim:
+        :return:
+        """
         command = "pyflow run --step_id \"{}\" --time {}"
         return command.format(step_id, timelim)
 
     @staticmethod
-    def get_handle_command(step_id: str):
+    def get_handle_command(step_id: str) -> str:
+        """
+        Command used for handling a completed array calculation.
+        :param step_id:
+        :return:
+        """
         command = "pyflow handle --step_id \"{}\""
+        return command.format(step_id)
+
+    @staticmethod
+    def get_begin_step_command(step_id: str) -> str:
+        """
+        Command used to begin a workflow step.
+        :param step_id:
+        :return:
+        """
+        command = "pyflow begin --step_id \"{}\""
         return command.format(step_id)
